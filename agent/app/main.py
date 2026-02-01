abcbc487eb559b0e3cef105f3bcffc8fecd282a4
k0ra09 <forkora@gmail.com>
2026-02-01 22:21:15 +0300

    Update main.py

--- agent/app/main.py
+++ agent/app/main.py
@@ -1,5 +1,4 @@
 import os
-import time
 import socket
 import asyncio
 from fastapi import FastAPI, Request, HTTPException
@@ -13,30 +12,25 @@ AGENT_TOKEN = os.getenv("AGENT_TOKEN")
 app = FastAPI()
 
 
-# ---------- AUTH ----------
+@app.on_event("startup")
+async def on_startup():
+    asyncio.create_task(register_loop())
+
+
 def check_auth(request: Request):
     auth = request.headers.get("Authorization")
     if auth != f"Bearer {AGENT_TOKEN}":
         raise HTTPException(status_code=401, detail="Unauthorized")
 
 
-# ---------- STATUS ----------
 @app.get("/status")
 async def status(request: Request):
     check_auth(request)
     
-    # Используем обновленную функцию метрик
     metrics = await collect_metrics()
     
     return {
         "node": AGENT_NAME,
         "status": "ok",
-        **metrics # Распаковываем time, system, services
+        **metrics
     }
-
-
-# ---------- STARTUP ----------
-@app.on_event("startup")
-async def on_startup():
-    # Запускаем цикл регистрации в фоне
-    asyncio.create_task(register_loop())
